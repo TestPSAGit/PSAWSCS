912035e2607119930ed78396e314388ce8e527d4
E562418 <E562418@B1009809.inetpsa.com>
2019-04-24 10:05:44 +0200

    test

---
+++ WebCarStore/src/test/java/Methodes/test.java
@@ -0,0 +1,15 @@
+/*
+ * Creation : 24 avr. 2019
+ */
+package Methodes;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+    @Test
+
+    public void ThePageTitleisCorrect() {
+        assert (1 == 2);
+    }
+}
